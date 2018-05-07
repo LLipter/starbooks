9e77de49595037ba1d8c5681670354792f3cb200
irran <irran@irrans-MBP.mshome.net>
2018-05-08 03:42:17 +0800

    validate user through database

--- src/table.sql
+++ src/table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE user(
     user_id INT auto_increment,
-    user_name VARCHAR(50) NOT NULL,
+    user_name VARCHAR(50) UNIQUE NOT NULL,
     passwd VARCHAR(50) NOT NULL,
     register_time date NOT NULL,
     PRIMARY KEY(user_id)
 );
+
+INSERT INTO user(user_name,passwd,register_time) VALUES("test","123456",NOW());
