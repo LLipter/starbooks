18cf0ebb905bec87ba03b8750292f76fd52cfbe8
irran <irran@irrans-MBP.mshome.net>
2018-05-15 00:55:23 +0800

    refactor files to make project well-organized

---
+++ login.jsp
@@ -0,0 +1,58 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Welcome to starbooks</title>
+<link rel="stylesheet" type="text/css" href="resource/css/login.css">
+</head>
+<body>
+	<div class="heading_container">
+		<div class="heading">
+			<!-- <p>Welcome to Starbooks!</p> -->
+		</div>
+	</div>
+	<div class="login_container">
+		<div class="login">
+			<!-- <form action="login" method="post">
+                userName:
+                <input type="text" name="user_name"> password:
+                <input type="password" name="passwd">
+                <input type="submit">
+            </form> -->
+			<form action="login" method="post" class="white-pink">
+				<h1>
+					Login to Starbooks <span>Please fill all the texts in the
+						fields.</span>
+				</h1>
+				<label> <span>User Name :</span> <input type="text"
+					name="user_name" />
+				</label> <label> <span>Password :</span> <input type="password"
+					name="passwd" />
+				</label> <label>
+					<p class="message">
+						<% 
+							Integer res = (Integer) request.getAttribute("loginResult");
+							if(res != null){
+								if((int)res == 1){
+									out.println("Hello, " + (String)request.getAttribute("user_name"));
+									out.println("your password is " + (String)request.getAttribute("passwd"));
+								}else if((int)res == 2){
+									out.println("No such user");
+								}else{
+									out.println("Invalid password");
+								}
+							}
+						%>
+					</p>
+				</label> <label>
+					<div class="button_container">
+						<input type="submit" class="button" />
+					</div>
+				</label>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,39 +16,41 @@ import java.sql.ResultSet;
 public class Login extends HttpServlet{
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html");
-		PrintWriter out = resp.getWriter();
-		out.println("<html>");
-		out.println("<body>");
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {	
 		String userName = req.getParameter("user_name");
 		String passwd = req.getParameter("passwd");
-		
 		ServletContext sc = getServletContext();
 		Connection con = (Connection) sc.getAttribute("DBCon");
+
 		try {
 			Statement st = con.createStatement();
 			String cmd = "SELECT passwd FROM user WHERE user_name = '" + userName + "'";
 			ResultSet rs = st.executeQuery(cmd);
 			if(!rs.next()) {
-				out.println("No such user");
+				req.setAttribute("loginResult", 2); // no such user
 			}else {
 				String truePasswd = rs.getString("passwd");
 				if(truePasswd.equals(passwd)) {
-					out.println("Hello, " + userName);
-					out.println("your password is " + passwd);
+					// login successfully
+					req.setAttribute("loginResult", 1);
+					req.setAttribute("user_name", userName);
+					req.setAttribute("passwd", passwd);
 				}else {
-					out.println("Invalid password");
+					req.setAttribute("loginResult", 3); // invalid password
 				}
 			}
+			RequestDispatcher view = req.getRequestDispatcher("login.jsp");
+			view.forward(req, resp);
 		} catch (SQLException e) {
+			PrintWriter out = resp.getWriter();
 			out.println("sql querying failed");
 			out.println(e.getMessage());
 			e.printStackTrace();
 		}
 		
-		out.println("</body>");
-		out.println("</html>");
+		
+
+
 	}
 	
 }
