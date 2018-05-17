aa78443876212dcbb56b079be8b641a4cc995629
irran <irran@irrans-MacBook-Pro.local>
2018-05-17 13:36:21 +0800

    modify User.java

--- src/model/User.java
+++ src/model/User.java
@@ -5,60 +5,50 @@ import java.sql.Timestamp;
 public class User {
 
 	private int user_id;
-	private String userName;
+	private String user_name;
 	private String passwd;
 	private Timestamp registerTime;
 	private int privilege;
-	
-	public User(int user_id, String userName, String passwd, Timestamp registerTime, int privilege) {
+	public User(int user_id, String user_name, String passwd, Timestamp registerTime, int privilege) {
 		super();
 		this.user_id = user_id;
-		this.userName = userName;
+		this.user_name = user_name;
 		this.passwd = passwd;
 		this.registerTime = registerTime;
 		this.privilege = privilege;
 	}
-
 	public int getUser_id() {
 		return user_id;
 	}
-
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
-
-	public String getUserName() {
-		return userName;
+	public String getUser_name() {
+		return user_name;
 	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
 	}
-
 	public String getPasswd() {
 		return passwd;
 	}
-
 	public void setPasswd(String passwd) {
 		this.passwd = passwd;
 	}
-
 	public Timestamp getRegisterTime() {
 		return registerTime;
 	}
-
 	public void setRegisterTime(Timestamp registerTime) {
 		this.registerTime = registerTime;
 	}
-
 	public int getPrivilege() {
 		return privilege;
 	}
-
 	public void setPrivilege(int privilege) {
 		this.privilege = privilege;
 	}
 	
+
 	
 	
 
