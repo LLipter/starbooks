d6fa1d605a67a509514aaaffc488af097824ab71
ZZisGreat <1148319421@qq.com>
2018-05-27 00:09:07 +0800

    update books.sql

--- src/books.sql
+++ src/books.sql
@@ -18,4 +18,23 @@ INSERT INTO book(book_name,author,publisher,price,res_url)
 VALUES('算法导论第三版中文(原书第3版)','Thomas H.Cormen','机械工业出版社',115.5,'sfdl8b7xt49f7f67s');
 INSERT INTO book(book_name,author,publisher,price,res_url) 
 VALUES('纳兰词/中华经典藏书','[清] 纳兰性德 ','吉林美术出版社',28.1,'TB1Lo0ickfb_uJkS0');
-
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('房思琪的初恋乐园','林奕含','北京联合出版公司',39.6,'5a45dc0cNf1c081f0');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('无声告白','[美] 伍绮诗','江苏凤凰文艺出版社',24.2,'5a659dccN8253acf2');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('我高兴死了','[美] 珍妮·罗森','江苏文艺出版社',44.9,'5aaf2c27Ne541d77e');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('有限与无限的游戏','[美] 詹姆斯·卡斯 ','电子工业出版社',27.5,'54c240e1Nde112061');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('传奇布列松','[法] 克莱蒙·舍卢','中国摄影出版社',217.4,'55b8679aN34ab9ea2');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('概念图','[美] Joseph D.Novak','人民邮电出版社',46.5,'579a2d03Nbe7a61b7');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('就喜欢你看不惯我又干不掉我的样子','白茶','长江文艺',27.6,'592bf16bNee823dc7');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('围城','钱钟书','人民文学出版社',25.5,'594a3c05N4d344e33');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('别说你懂思维导图','赵国庆','人民邮电出版社',115.5,'5534a2f1Nc75bbd54');
+INSERT INTO book(book_name,author,publisher,price,res_url) 
+VALUES('荒野之歌','[英] 罗莎蒙德·基德曼·考克斯','北京联合出版公司',171.0,'5928e0c3Nde50bdae');
