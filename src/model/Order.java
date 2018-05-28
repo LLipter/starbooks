be350d90fc297cce4e67c5f848dfbd704828af05
LLran <katsudonn@163.com>
2018-05-28 23:57:36 +0800

    fix totalPrice bug

--- src/model/Order.java
+++ src/model/Order.java
@@ -10,7 +10,6 @@ public class Order implements Iterable<Item>{
     private ArrayList<Item> items;
     private Timestamp created_time;
     private int order_status;
-    private double totalPrice;
     
     public Order() {
     	// do nothing
@@ -20,7 +19,6 @@ public class Order implements Iterable<Item>{
     	items = new ArrayList<Item>();
 		this.created_time = created_time;
 		this.order_status = order_status;
-		totalPrice = 0;
 	}
 
 	public int getOrder_status() {
@@ -36,12 +34,15 @@ public class Order implements Iterable<Item>{
 	}
 	
 	public double getTotalPrice() {
-		return totalPrice;
+		double ret = 0;
+		for(Item item : this) {
+			ret += item.getQuantity() * item.getBook().getPrice();
+		}
+		return ret;
 	}
 
 	public void AddItems(Item item) {
         this.items.add(item);
-        this.totalPrice += item.getBook().getPrice();
     }
 	
 	public void removeItem(Item item) {
