f91b3b0e455e14c932f886c288b7c4430f32af8c
HarryCheng <582416551@qq.com>
2018-05-28 18:31:28 +0800

    Single

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Book {
 
     private final int book_id;
@@ -15,8 +20,13 @@ public class Book {
     private final String res_url;
 
     private int book_status;
+    
+    private String txt;
+    
+ 
 
-    public Book(int book_id, String book_name, String author, String publisher, double price, String res_url, int book_status){
+    public Book(int book_id, String book_name, String author, String publisher,
+    		double price, String res_url, int book_status){
         this.book_id = book_id;
         this.book_name = book_name;
         this.author = author;
@@ -24,6 +34,7 @@ public class Book {
         this.price = price;
         this.res_url = res_url;
         this.book_status = book_status;
+     
     }
 
     public int getBook_id() {
@@ -53,7 +64,36 @@ public class Book {
     public int getBook_status() {
         return book_status;
     }
-
+   
+    public String getText(String url)  {
+        FileReader fr = null;
+		try {
+			fr = new FileReader(url);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+        BufferedReader br=new BufferedReader(fr);
+        txt=null;
+        
+        
+        try {
+			while(br.readLine()!=null){
+			    txt+=br.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+ 
+        try {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return txt;
+    }
+    
+    
     public void setBook_status(int book_status) {
         this.book_status = book_status;
     }
