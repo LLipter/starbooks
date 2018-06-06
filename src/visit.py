3b9d2cd1de86c6317bfc7fd9130aef35cb39cc77
LLran <katsudonn@163.com>
2018-06-06 22:29:15 +0800

    modify visit.py

--- src/visit.py
+++ src/visit.py
@@ -1,11 +1,16 @@
 import urllib.request
 import time
+import os
+import sys
+
+pid = os.fork()
+if pid:
+    sys.exit(0)
+os.chdir('/')
+os.setsid()
 
 url = "http://www.irran.top:8080/starbooks"
 while True:
     html = urllib.request.urlopen(url)
     # print(html)
     time.sleep(60 * 60) # visit this website every 1 hour
-
-# execute it in background
-# python3 visit.py &
